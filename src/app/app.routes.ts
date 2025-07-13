671d5aa5573bf498b18b4f938e93c5d4d9d5eec1
Eduards <es23151@edu.lu.lv>
2025-07-13 21:20:57 +0300

    feat: Implement multi-page routing structure (TM-10 to TM-13)
    - Created a dedicated page for the task creation form at '/create-task' (TM-11).
    - Created a dedicated page for the task list at '/task-list' (TM-12).
    - Implemented a dynamic task details page using route parameters at '/task-list/:id' (TM-13).
    - Styled a reusable HeaderComponent for consistent navigation.
    - Changed px to em in stylessheets

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { CreateTask } from './components/create-task/create-task';
+import path from 'path';
+import { Component } from '@angular/core';
+import { TaskList } from './components/task-list/task-list';
+import { TaskDetails } from './components/task-details/task-details';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     {
+    path: '',
+    redirectTo: '/task-list',
+    pathMatch: 'full'
+  },
+    {
+        path: 'create-task',
+        component: CreateTask,
+    },
+    { 
+    path: 'task-list',
+    component: TaskList
+    },
+    {
+        path: 'task-list/:id', component: TaskDetails
+    }
+];
---
+++ src/app/components/task-list/task-list.ts
@@ -0,0 +1,43 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Task } from '../../models/task.model';
+import { RouterLink } from '@angular/router';
+
+
+@Component({
+  selector: 'app-task-list',
+  imports: [CommonModule, RouterLink],
+  templateUrl: './task-list.html',
+  styleUrl: './task-list.scss'
+})
+export class TaskList implements OnInit {
+  taskList: Task[] = [];
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    // We call http.get() and tell it to expect an array of Tasks (Task[])
+    // The path '/assets/tasks.json' points file
+    this.http.get<Task[]>('assets/tasks.json').subscribe((loadedTasks)=>{
+      this.taskList = loadedTasks.map(task => ({
+        ...task,
+        createdOn: new Date(task.createdOn)
+      }));
+
+      console.log('Tasks loaded from JSON file: ', this.taskList)
+    });
+
+  }
+
+  addTaskToList(newTask: Task): void {
+    // this.taskList.push(newTask);
+    this.taskList = [newTask, ...this.taskList];
+    console.log('Task recived by parent: ', this.taskList);
+  }
+  deleteTask(taskToDelete: Task): void {
+    this.taskList = this.taskList.filter(task => task.id !== taskToDelete.id);
+
+    console.log('Task deleted: ', this.taskList);
+  }
+}
