02f0a3c100b8be2cbcefad3eadc8706224df5f6f
Eduards <es23151@edu.lu.lv>
2025-07-15 21:24:08 +0300

    TM-6: Able to read and display json file

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 import { CreateTask } from "./components/create-task/create-task";
 import { TaskList } from "./components/task-list/task-list";
 import { Task } from './models/task.model';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CreateTask, TaskList],
+  imports: [RouterOutlet, CreateTask, TaskList, CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
-export class App {
+export class App implements OnInit {
   protected title = 'bootcamp';
 
   tasks: Task[] = [];
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.http.get<Task[]>('assets/tasks.json').subscribe((loadedTasks) =>{
+      this.tasks = loadedTasks;
+      console.log('Tasks loaded from JSON:', this.tasks);
+    });
+  }
 
   onTaskAdded(newTask: Task): void{
     this.tasks.push(newTask);
