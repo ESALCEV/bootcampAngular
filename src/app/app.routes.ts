1e44650c2e0fe72685a1cfba83d989170b62ae5f
Dudix2 <112017552+Dudix2@users.noreply.github.com>
2025-07-18 22:34:39 +0300

    TM-11: Created a 'Create Task' page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { CreateTaskComponent } from './features/tasks/components/create-task/create-task.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: '/create',
+        pathMatch: 'full'
+    },
+    {
+        path: 'create',
+        component: CreateTaskComponent
+    }
+];
