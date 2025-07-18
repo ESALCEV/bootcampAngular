05776a49c66c07cf33f7e559556515977927c3f3
Dudix2 <112017552+Dudix2@users.noreply.github.com>
2025-07-18 22:50:24 +0300

    TM-12: Created a 'Task List' page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,19 @@
 import { Routes } from '@angular/router';
 import { CreateTaskComponent } from './features/tasks/components/create-task/create-task.component';
+import { TaskListComponent } from './features/tasks/components/task-list/task-list.component';
 
 export const routes: Routes = [
     {
         path: '',
-        redirectTo: '/create',
+        redirectTo: '/tasks',
         pathMatch: 'full'
     },
     {
         path: 'create',
         component: CreateTaskComponent
+    },
+    {
+        path: 'tasks',
+        component: TaskListComponent
     }
 ];
