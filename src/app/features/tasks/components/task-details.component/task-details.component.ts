f1ba0a1bbb8d07415478e6191a41c3c66925c244
Eduards Salcevics <eduards.salcevics@oc.vasscompany.com>
2025-09-17 17:47:10 +0300

    TM-32: Allow editing of task details in the detail view

--- src/app/features/tasks/components/task-details.component/task-details.component.ts
+++ src/app/features/tasks/components/task-details.component/task-details.component.ts
@@ -1,13 +1,16 @@
-import { Component, inject } from '@angular/core';
+import { Component, effect, inject, signal } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { TaskService } from '../../services/task.service';
-import { switchMap, EMPTY } from 'rxjs';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { switchMap, EMPTY, combineLatest } from 'rxjs';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
 import { UserService } from '../../../users/services/user.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Task, TASK_STATUSES, TASK_TYPES, UNASSIGNED } from '../../models/task.model';
 
 @Component({
-  selector: 'app-task-details.component',
+  selector: 'app-task-details',
   templateUrl: './task-details.component.html',
+  styleUrl: '../create-task/create-task.component.scss',
   standalone: false
 })
 
@@ -16,10 +19,23 @@ export class TaskDetailsComponent {
   private router = inject(Router);
   private taskService = inject(TaskService);
   private userService = inject(UserService);
+  private fb = inject(FormBuilder);
+
+  users = toSignal(this.userService.getUsers(), { initialValue: [] });
+  isEditing = signal(false);
+
+  refreshPage = signal(0);
+
+  taskForm: FormGroup;
+  types = TASK_TYPES;
+  statuses = TASK_STATUSES;
 
   task = toSignal(
-    this.route.paramMap.pipe(
-      switchMap(params => {
+    combineLatest([
+      this.route.paramMap,
+      toObservable(this.refreshPage)
+    ]).pipe(
+      switchMap(([params]) => {
         const id = params.get('id');
         if (id) {
           return this.taskService.getTaskbyId(id);
@@ -28,7 +44,51 @@ export class TaskDetailsComponent {
       })
     )
   );
-  
+
+  constructor() {
+    this.taskForm = this.fb.group({
+      title: ['', [Validators.required, Validators.minLength(3)]],
+      description: [''],
+      type: ['', Validators.required],
+      status: ['', Validators.required],
+      assignedTo: [UNASSIGNED, Validators.required],
+    })
+      effect(() => {
+        const currentTask = this.task();
+        if (currentTask && this.isEditing()) {
+          this.taskForm.patchValue(currentTask);
+        }
+    });
+  }
+
+  onEditClick() {
+    this.isEditing.set(true);
+  }
+
+  onSave() {
+    if (this.taskForm.invalid) {
+      this.taskForm.markAllAsTouched();
+      return;
+    }
+
+    const currentTask = this.task();
+    if (!currentTask) return;
+
+    const updatedTask: Task = { ...currentTask, ...this.taskForm.value };
+
+    this.taskService.updateTask(updatedTask).subscribe({
+      next: () => {
+        this.isEditing.set(false);
+        this.refreshPage.update(count => count + 1);
+      },
+      error: (err) => console.error('Error updating task:', err),
+    });
+  }
+
+  onCancel() {
+    this.isEditing.set(false);
+  }
+
   goBack(): void {
     this.router.navigate(['/']);
   }
