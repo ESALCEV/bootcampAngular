cbca46c3e2cec69636fc579f26a477d38206cc97
Eduards Salcevics <eduards.salcevics@oc.vasscompany.com>
2025-10-27 12:16:53 +0200

    TM-40: Added role permissions

--- src/app/features/tasks/components/task-list/task-list.component.ts
+++ src/app/features/tasks/components/task-list/task-list.component.ts
@@ -2,22 +2,33 @@ import { Component, computed, inject, signal } from '@angular/core';
 import { TaskService } from '../../services/task.service';
 import { Task } from '../../models/task.model';
 import { rxResource } from '@angular/core/rxjs-interop';
+import { AuthService } from '../../../auth/services/auth.service';
+import { UserRole } from '../../../users/models/user.model';
 
 @Component({
   selector: 'app-task-list',
   templateUrl: './task-list.component.html',
   styleUrl: './task-list.component.scss',
-  standalone: false
+  standalone: false,
 })
-export class TaskListComponent {  
+export class TaskListComponent {
   private taskService = inject(TaskService);
+  authService = inject(AuthService);
 
   private refreshTrigger = signal(0);
+
+  isAdmin(): boolean {
+    if (this.authService.currentUser()?.roles.includes(UserRole.ADMIN)) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 
   tasksResource = rxResource<Task[], number>({
     params: () => this.refreshTrigger(),
     stream: () => this.taskService.getTasks(),
-    defaultValue: []
+    defaultValue: [],
   });
 
   tasks = computed(() => this.tasksResource.value());
@@ -25,10 +36,10 @@ export class TaskListComponent {
   isLoading = computed(() => this.tasksResource.status() === 'loading');
   hasError = computed(() => this.tasksResource.status() === 'error');
 
-  deleteTask(taskId: string): void{
+  deleteTask(taskId: string): void {
     this.taskService.deleteTask(taskId).subscribe({
       next: () => {
-        this.refreshTrigger.update(count => count + 1);
+        this.refreshTrigger.update((count) => count + 1);
       },
       error(err) {
         console.error(`Failed to delete task ${taskId}`, err); // in real app would be err msg to user
